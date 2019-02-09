70bc6e33a2571f54e6cf7df5cb5404afd14745ea
MorganJamesSmith <Morgan.J.Smith@outlook.com>
2019-02-09 18:41:34 -0500

    Made a canvas in pong

--- pipepong/src/components/Pong.tsx
+++ pipepong/src/components/Pong.tsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class Pong extends Component {
+  ctx?: CanvasRenderingContext2D;
   render() {
     return (
       <div className="pong">
-        <h1> Hello this is me </h1>
+        <canvas id="myCanvas" ref="myCanvas" width="480" height="320"></canvas>
       </div>
     );
+  }
+  componentDidMount(){
+    const ctx = (this.refs.myCanvas as HTMLCanvasElement).getContext("2d");
+    if(ctx !== null){
+        this.ctx = (ctx as CanvasRenderingContext2D);
+    } else {
+        throw "NO CONTEXT";
+    }
+    this.ctx.beginPath();
+    this.ctx.moveTo(100, 100);
+    this.ctx.lineTo(100, 200);
+    this.ctx.stroke();
+    this.ctx.closePath();
   }
 }
 
