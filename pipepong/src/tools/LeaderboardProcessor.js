0ae0d2b5132d68cd8f4f81de5f2952192e6ee865
matt1light <matt1light@gmail.com>
2019-02-10 12:04:42 -0500

    sort after timealive calculation

--- pipepong/src/tools/LeaderboardProcessor.js
+++ pipepong/src/tools/LeaderboardProcessor.js
@@ -1,12 +1,13 @@
 function processLeaderBoardList(leaderboard, height){
-    leaderboard.sort(compareLifetimes);
     var total = leaderboard.sum("timeAlive");
     var offset = 0;
     leaderboard.map((player)=>{
+        player.timeAlive = Date.now() - player.time
         player.height = height*player.timeAlive/total;
         player.y = offset;
         offset += player.height;
     })
+    leaderboard.sort(compareLifetimes);
 }
 
 function compareLifetimes(b, a){
