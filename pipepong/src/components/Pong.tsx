6a2fbc2c83f868d8dcbb681bb759541e23d2acb7
Tadhg <tadhgmister@gmail.com>
2019-02-10 08:03:36 -0500

    Now paddle moves side to side
    input has been implemented, need to fix paddle so it takes the VERTICAL position as a props.

--- pipepong/src/components/Pong.tsx
+++ pipepong/src/components/Pong.tsx
@@ -4,16 +4,50 @@ import Paddle from './Paddle'
 
 const PLAYHEIGHT = 400;
 const PLAYWIDTH = 800;
-class Pong extends Component {
+
+interface PongProps {
+
+}
+interface PongState {
+  paddleDistFromWall: number;
+}
+class Pong extends Component<PongProps, PongState> {
+  fieldInput: HTMLInputElement | null = null;
+  constructor(props:PongProps){
+    super(props);
+    this.state = {
+      paddleDistFromWall: 20
+    };
+  }
   render() {
     return (
         <div style={{height:""+PLAYHEIGHT+"px", width: ""+PLAYWIDTH+"px", border: "solid"}}>
            <Ball minX={0} maxX={PLAYWIDTH} minY={-PLAYHEIGHT} maxY={0} dx={4} dy={-4}/>
-           <Paddle playHeight={PLAYHEIGHT} distanceFromWall={20}/>
+           <Paddle playHeight={PLAYHEIGHT} distanceFromWall={this.state.paddleDistFromWall}/>
+           <input 
+              ref={(input => this.fieldInput=input)}
+              type="text" 
+              placeholder="click here to play" 
+              value="" 
+              // onKeyPress = {(e)=>this.handleInput(e)}
+              onChange = {(e)=>this.handleInput(e)}
+            ></input>
     </div>
     );
   }
   componentDidMount(){
+    if(this.fieldInput !== null){this.fieldInput.focus()}
+  }
+  handleInput(e:React.ChangeEvent<HTMLInputElement>){
+    let move = 0;
+    if (e.target.value == "j"){ 
+      move = 5; // move down screen
+    } else if (e.target.value == "k") { 
+      move = -5; // move up screen 
+    }
+    this.setState({
+      paddleDistFromWall: this.state.paddleDistFromWall + move
+    })
   }
 }
 
