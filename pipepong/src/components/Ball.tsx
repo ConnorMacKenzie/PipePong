1f7ba8c1da1b7d279e8ea490789515b9186ae8e8
MorganJamesSmith <Morgan.J.Smith@outlook.com>
2019-02-10 08:42:29 -0500

    moved ball logic from ball to pong

--- pipepong/src/components/Ball.tsx
+++ pipepong/src/components/Ball.tsx
@@ -3,53 +3,24 @@ import React, { Component } from 'react';
 const SHAPEWIDTH = 40;
 
 interface CLASSProps {
-    minX : number;
-    maxX : number;
-    minY : number;
-    maxY : number;
-    dx : number;
-    dy : number;
+    curX : number;
+    curY : number;
 }
 
 interface CLASSState {
-    curX : number;
-    curY : number;
-    dx : number;
-    dy : number;
-    minX : number;
-    maxX : number;
-    minY : number;
-    maxY : number;
-
 }
 class Ball extends React.Component<CLASSProps, CLASSState> {
   constructor(props : any) {
     super(props);
     this.state = {
-      curX : 0,
-      curY : 0,
-      dx : props.dx,
-      dy : props.dy,
-      minX : props.minX,
-      maxX : props.maxX - SHAPEWIDTH,
-      minY : props.minY + SHAPEWIDTH,
-      maxY : props.maxY,
     };
-  }
-  repositionBall() {
-    this.setState({
-        curX : this.state.curX + this.state.dx,
-        curY : this.state.curY + this.state.dy,
-        dx : (this.state.curX + this.state.dx > this.state.maxX || this.state.curX + this.state.dx < this.state.minX ? this.state.dx * -1 : this.state.dx),
-        dy : (this.state.curY + this.state.dy > this.state.maxY || this.state.curY + this.state.dy < this.state.minY ? this.state.dy * -1 : this.state.dy)
-    });
   }
 
   render() {
     let Ballstyle : React.CSSProperties = {
         position: "relative",
-        bottom: ""+this.state.curY+"px",
-        left: ""+this.state.curX+"px",
+        top: ""+this.props.curY+"px",
+        left: ""+this.props.curX+"px",
         display: "block"
     }
     return (
@@ -57,9 +28,6 @@ class Ball extends React.Component<CLASSProps, CLASSState> {
         <circle cx={"" + SHAPEWIDTH/2 + "px"} cy={"" + SHAPEWIDTH/2 + "px"} r={"" + SHAPEWIDTH/2 + "px"} fill="pink" />
       </svg>
     );
-  }
-  componentDidMount(){
-    setInterval(() => this.repositionBall() ,10);
   }
 }
 
--- pipepong/src/components/Pong.tsx
+++ pipepong/src/components/Pong.tsx
@@ -4,31 +4,52 @@ import Paddle from './Paddle'
 
 const PLAYHEIGHT = 400;
 const PLAYWIDTH = 800;
+const SHAPEWIDTH = 40;
 
 interface PongProps {
 
 }
 interface PongState {
   paddleDistFromWall: number;
+  curX : number;
+  curY : number;
+  dx : number;
+  dy : number;
 }
 class Pong extends Component<PongProps, PongState> {
   fieldInput: HTMLInputElement | null = null;
   constructor(props:PongProps){
     super(props);
     this.state = {
-      paddleDistFromWall: 20
+      paddleDistFromWall: 20,
+      curX : 0,
+      curY : 0,
+      dx : 4,
+      dy : -4,
     };
   }
+
+
+  repositionBall() {
+      this.setState({
+          curX : this.state.curX + this.state.dx,
+          curY : this.state.curY + this.state.dy,
+        dx : (this.state.curX + this.state.dx + SHAPEWIDTH > PLAYWIDTH || this.state.curX + this.state.dx < 0 ? this.state.dx * -1 : this.state.dx),
+        dy : (this.state.curY + this.state.dy + SHAPEWIDTH > PLAYHEIGHT || this.state.curY + this.state.dy < 0 ? this.state.dy * -1 : this.state.dy)
+  });
+}
+
+
   render() {
     return (
         <div style={{height:""+PLAYHEIGHT+"px", width: ""+PLAYWIDTH+"px", border: "solid"}}>
-           <Ball minX={0} maxX={PLAYWIDTH} minY={-PLAYHEIGHT} maxY={0} dx={4} dy={-4}/>
+           <Ball curX={this.state.curX} curY={this.state.curY}/>
            <Paddle playHeight={PLAYHEIGHT} distanceFromWall={this.state.paddleDistFromWall}/>
-           <input 
+           <input
               ref={(input => this.fieldInput=input)}
-              type="text" 
-              placeholder="click here to play" 
-              value="" 
+              type="text"
+              placeholder="click here to play"
+              value=""
               // onKeyPress = {(e)=>this.handleInput(e)}
               onChange = {(e)=>this.handleInput(e)}
             ></input>
@@ -37,13 +58,14 @@ class Pong extends Component<PongProps, PongState> {
   }
   componentDidMount(){
     if(this.fieldInput !== null){this.fieldInput.focus()}
+    setInterval(() => this.repositionBall() ,10);
   }
   handleInput(e:React.ChangeEvent<HTMLInputElement>){
     let move = 0;
-    if (e.target.value == "j"){ 
+    if (e.target.value == "j"){
       move = 5; // move down screen
-    } else if (e.target.value == "k") { 
-      move = -5; // move up screen 
+    } else if (e.target.value == "k") {
+      move = -5; // move up screen
     }
     this.setState({
       paddleDistFromWall: this.state.paddleDistFromWall + move
