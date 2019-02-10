c130c4e872b6e3abcad908cef34574ba2f56ab2b
matt1light <matt1light@gmail.com>
2019-02-10 07:49:27 -0500

    send login publish message on login

--- pipepong/src/App.tsx
+++ pipepong/src/App.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Pong from './components/Pong';
 import Login from './components/LoginPage';
+import CommHandler from './comms/CommHandler';
+import {v4} from 'uuid'
 
 interface CLASSProps {
 }
@@ -10,16 +12,21 @@ interface CLASSState {
     joined: Boolean;
     name: string;
     color: string;
+    uuid: string;
 }
 
 class App extends React.Component<CLASSProps, CLASSState> {
+    commHandler:CommHandler;
     constructor(props: CLASSProps) {
         super(props);
         this.state = {
             joined: false,
             name: "",
-            color: "#00FF00"
+            color: "#00FF00",
+            uuid: "",
         }
+        this.commHandler = new CommHandler();
+        this.commHandler.connect();
     };
 
 
@@ -27,8 +34,12 @@ class App extends React.Component<CLASSProps, CLASSState> {
     this.setState({
         joined: true,
         name:name,
-        color: color
+        color: color,
+        uuid: v4(),
     });
+    this.commHandler.publishJoin(this.state.uuid,
+                                 this.state.name,
+                                 this.state.color)
   }
 
   render() {
