2208a0eeb7c09ad3d83b7c0a6e7249c51b6ac996
Tadhg <tadhgmister@gmail.com>
2019-02-09 21:52:21 -0500

    added template tsx file
    added a template file that includes demo for how to include properties and state.

---
+++ pipepong/src/TEMPLATE.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+interface CLASSProps {
+    // properties this component recieves from parent here
+}
+
+interface CLASSState {
+    // any variables that can affect the UI (when updated should re-render)
+
+}
+
+class CLASS extends React.Component<CLASSProps, CLASSProps> {
+    public render(){
+        return (
+            <div className = "CLASS">
+                {/* stuff here */}
+            </div>
+        )
+    }
+}
+
+
+export default CLASS;
